1b56d513b0a8ee05c14806f28c1e828d14814b54
Angie Long <ylong@ucsd.edu>
2020-01-23 11:16:44 -0800

    Lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,20 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		//$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Javascript has taken control");
+		$("jumbotron p").addClass("active");
+		$(". jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+}
+
+function projectClick(){
+
+	var containingProject = $(this).closest(".project");
+	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	//$(#"myelement").toggle()
 }
